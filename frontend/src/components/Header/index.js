3dd88affb291989d7a480ef2dcf411fd430b794d
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-01-23 19:34:45 -0800

    Cleanup + test that the frontend can get data from API

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import { Wrapper, Content } from "./Header.styles";
 
-const Header = () => (
-    <Wrapper>
-        <Content>
-            <h1>Ptracker</h1>
-        </Content>
-    </Wrapper>
-);
+function Header() {
+
+    const [message, setMessage] = useState();
+
+    useEffect(() => {
+        let mounted = true;
+        fetch('http://127.0.0.1:9000')
+            .then(data => data.json())
+            .then(data => {
+                if (mounted) {
+                    setMessage(data.message);
+                }
+            })
+        return () => mounted = false;
+    }, [])
+
+    return (
+        <Wrapper>
+            <Content>
+                <h1>Ptracker {message}</h1>
+            </Content>
+        </Wrapper>
+    )
+}
 
 export default Header
