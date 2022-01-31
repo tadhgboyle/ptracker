4dd40cf6b1765d8276f1ba31a0d3941a074dd053
chris <77702776+cchan45@users.noreply.github.com>
2022-01-31 15:38:23 -0800

    random

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,47 @@
 const createError = require('http-errors');
+const path = require('path')
+
+//Express server
 const express = require('express');
 const cookieSession = require('cookie-session');
+const session = require('express-session');
+const ejsLayouts = require('express-ejs-layouts');
+
+//middleware and routes
+const passport = require('./auth/passport');
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+
+//Loggers, cors, and swagger
 const logger = require('morgan');
 const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
-require('./auth/passportGoogle');
+require('./auth/passport');
 
 app.use(logger('dev'));
+app.use(ejsLayouts)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY],
 }));
+app.set('view engine', 'ejs');
 app.use(cors());
-app.use(session({ secret: 'melody hensley is my spirit animal' }));
+app.use(session({ 
+    secret: 'melody hensley is my spirit animal',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000
+    }}
+));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -43,9 +64,6 @@ app.use(
     swaggerUi.serve,
     swaggerUi.setup(openapiSpecification)
 );
-
-const authRouter = require('./routes/authRoutes');
-const userRouter = require('./routes/userRoutes');
 
 app.use('/auth', authRouter);
 app.use('/user', userRouter);
