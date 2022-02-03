04a89fa1f0e3337c4d55d2b68ac80b848c5fabf7
Martin <martinlee5159@gmail.com>
2022-02-03 15:53:42 -0800

    add modal for adding shifts

--- public/calendar.js
+++ public/calendar.js
@@ -1,38 +1,78 @@
+function openModal(key) {
+  document.getElementById(key).showModal(); 
+  document.getElementById(key).children[0].scrollTop = 0; 
+  document.getElementById(key).children[0].classList.remove('opacity-0'); 
+  document.getElementById(key).children[0].classList.add('opacity-100')
+}
+
+function modalClose(key) {
+  document.getElementById(key).children[0].classList.remove('opacity-100');
+  document.getElementById(key).children[0].classList.add('opacity-0');
+  setTimeout(function () {
+      document.getElementById(key).close();
+      document.body.removeAttribute('style');
+  }, 100);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   let startDate = new Date()
   
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialDate: startDate.toISOString().split('T')[0],
+    fixedWeekCount: false,
     editable: true,
     selectable: true,
     businessHours: true,
-    dayMaxEvents: true, // allow "more" link when too many events
+    dayMaxEvents: true, // allow "more" button when too many shifts
+    
+    // Callback for when you click a date
+    dateClick: function(info) {
+      openModal('addShiftModal')
+      document.getElementById('shiftDatePicker').value = info.dateStr
+    },
+    // Static shifts
     events: [
       {
         title: 'Example Shift (D)',
-        start: '2022-02-01'
+        start: '2022-02-04',
+        color: '#ECA446',
+       
       },
       {
         title: 'Example Shift (E)',
-        start: '2022-02-04'
+        start: '2022-02-05',
+        color: '#016BB7'
+      },
+      {
+        title: 'Example Shift (E)',
+        start: '2022-02-06',
+        color: '#016BB7'
+      },
+      {
+        title: 'Example Shift (E)',
+        start: '2022-02-04',
+        color: '#016BB7'
       },
       {
         title: 'Example Shift (N)',
-        start: '2022-02-05'
+        start: '2022-02-04',
+        color: '#744468'
       },
       {
-        title: 'Example Shift (S)',
-        start: '2022-02-06'
+        title: 'Sick Student (S)',
+        start: '2022-02-14',
+        color: '#D05353'
       },
       {
-        title: 'Multiple Shifts',
+        title: 'Holiday or Event',
         start: '2022-02-07',
-        end: '2022-02-10'
+        end: '2022-02-10',
+        color: '#577590'
       },
       {
         title: 'Shift with Time',
-        start: '2022-02-12T10:30:00',
+        start: '2022-02-12T07:30:00',
         end: '2022-02-12T12:30:00'
       },
     ]
