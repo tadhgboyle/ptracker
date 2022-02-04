70d5d929c004967f11107dbf01c3e7d7c88d54eb
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-02-04 10:26:43 -0800

    misc

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,39 +1,26 @@
 const express = require('express');
 
-// Prisma
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-
 // Authenication measures
 const {ensureAuthenticated, isInstructor, isAdmin} = require('../middleware/checkAuth')
 
 const router = express.Router();
 
+const Role = require('../models/Role');
+const User = require('../models/User');
+
 // Datetime
 let dt = new Date();
 let currentDays = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
 
-// Fetching all the users in the database
-const getAllUsers = async () => {
-    const users = await prisma.user.findMany({
-        include: {
-            section: true,
-            shift: true,
-        },
-    })
-    return users
-}
-
-
 router.get('/dashboard', ensureAuthenticated, async (req,res) => {
-    if (req.user.role == 'INSTRUCTOR') {
+    if (req.user.role === Role.INSTRUCTOR) {
         res.render('dashboard/dashboardProf', {
             page: 'dashboard',
             days: currentDays,
             monthYear: `${dt.toLocaleString('default', { month: 'long' })}, ${dt.getFullYear()}`,
             year: dt.getFullYear(),
             date: dt,
-            students: await getAllUsers()
+            students: await User.all(),
         });
     } else {
         res.render('dashboard/dashboard', {
