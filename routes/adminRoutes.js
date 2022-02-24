47eeac0da288600d5e1054ac34f3aaae8e118c54
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-02-23 18:35:00 -0800

    misc fixes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const {check, validationResult} = require("express-validator");
 const User = require("../models/user");
 const Section = require("../models/section");
+const Role = require("../models/role");
 
 router.post(
     '/changeRole',
@@ -24,6 +25,14 @@ router.post(
             req.session.error_message = `Invalid input: ${err}!`;
 
             return res.redirect('/admin');
+        }
+
+        if (req.body.role === Role.STUDENT) {
+            const section = await Section.whereIsInstructor(req.body.userId);
+            if (section) {
+                req.session.error_message = `User is currently assigned to section ${section.name}, and cannot be demoted!`;
+                return res.redirect('/admin');
+            }
         }
 
         await User.update(req.body.userId, {
@@ -113,6 +122,13 @@ router.post(
             req.session.error_message = `Instructor #${req.body.instructor} is already assigned another section`;
             return res.redirect('/admin');
         }
+
+        await User.prisma.user.update({
+            where: {id: parseInt(req.body.instructor)},
+            data: {
+                section: {connect: {id: section.id}}
+            }
+        });
 
         req.session.success_message = `Set instructor of section ${section.name} to #${req.body.instructor}`;
 
