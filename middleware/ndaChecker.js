ab937792382a282aead4bedf6af6a8396f5b3cf4
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-02-25 09:47:47 -0800

    maybe fix heroku

--- middleware/ndaChecker.js
+++ middleware/ndaChecker.js
@@ -2,12 +2,13 @@ const Role = require('../models/role');
 
 const ndaChecker = (req, res, next) => {
     if (req.user) {
-        if (req.user.section.id === 1 && req.user.role === Role.STUDENT) {
+        if (req.path.includes('auth')) {
+            return next();
+        } else if (req.user.section.id === 1 && req.user.role === Role.STUDENT) {
             res.render('pendingSection', {
                 page: 'assign',
             });
-        }
-        else if (req.user.acceptedNda || req.path.includes('nda') || req.path.includes('auth')) {
+        } else if (req.user.acceptedNda || req.path.includes('nda') || req.path.includes('auth')) {
             return next();
         } else {
             return res.redirect('/nda');
